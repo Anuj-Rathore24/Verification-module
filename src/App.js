87eaa8a7bad3072d8629fa4cba672c8d5cb22b18
Astha Mishra <mishraastha255@gmail.com>
2022-05-09 15:25:04 +0530

    User Dashboard (intial commit)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./styles/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import VerificationForm from "./pages/form/VerificationForm";
 import AdminDashboard from "./pages/admin-dashboard/AdminDashboard";
+import UserDashboard from "./pages/user-dashboard/UserDashboard";
 import Home from "./pages/Home.js";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="/form" element={<VerificationForm/>}/>
           <Route path="/admindashboard" element={<AdminDashboard/>}/>
+          <Route path="/UserDashboard" element={<UserDashboard/>}/>
         </Routes>
       </BrowserRouter>
     </div>
