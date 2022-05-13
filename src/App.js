8545cf6e58427dae18a16ad355e57d19c9f19916
Anuj-Rathore24 <anujsingh2409@gmail.com>
2022-05-13 09:40:47 +0530

    deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import "./styles/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import VerificationForm from "./pages/form/VerificationForm";
-<<<<<<< HEAD
-import Admin from "./pages/admin.js";
 import DocumentVerification from "./pages/DocumentVerification/DocumentVerification";
 import 'semantic-ui-css/semantic.min.css';
-=======
 import AdminDashboard from "./pages/admin-dashboard/AdminDashboard";
 import UserDashboard from "./pages/user-dashboard/UserDashboard";
 import Home from "./pages/Home.js";
->>>>>>> 7cbf46ed45e2b93dfdce911b2fb98a9bc7a01507
 
 function App() {
   return (
@@ -18,12 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/form" element={<VerificationForm/>}/>
-<<<<<<< HEAD
-          <Route path="/verifydoc" element={<DocumentVerification/>}/>
-=======
-          <Route path="/admindashboard" element={<AdminDashboard/>}/>
+          {/* <Route path="/verifydoc" element={<DocumentVerification/>}/> */}
+          {/* <Route path="/admindashboard" element={<AdminDashboard/>}/> */}
           <Route path="/UserDashboard" element={<UserDashboard/>}/>
->>>>>>> 7cbf46ed45e2b93dfdce911b2fb98a9bc7a01507
         </Routes>
       </BrowserRouter>
     </div>
