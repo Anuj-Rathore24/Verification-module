c5f5fedac51d8d7f5bc0391b07353054ea2e22fd
MonishK-1024 <kmonish72g@gmail.com>
2022-05-19 00:45:58 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import "./styles/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import VerificationForm from "./pages/form/VerificationForm";
-import DocumentVerification from "./pages/DocumentVerification/DocumentVerification";
-import 'semantic-ui-css/semantic.min.css';
-import AdminDashboard from "./pages/admin-dashboard/AdminDashboard";
-import UserDashboard from "./pages/user-dashboard/UserDashboard";
+import VerificationForm from "./pages/form/VerificationForm.js";
+import DocumentVerification from "./pages/DocumentVerification/DocumentVerification.js";
+// import 'semantic-ui-css/semantic.min.css';
+import AdminDashboard from "./pages/admin-dashboard/AdminDashboard.js";
+import UserDashboard from "./pages/user-dashboard/UserDashboard.js";
 import Home from "./pages/Home.js";
+import Login from './pages/login-signup/Login.jsx'
+import Signup from './pages/login-signup/Signup.jsx'
+import OTP from './pages/login-signup/OTP.jsx'
 
 function App() {
   return (
-    <div className="App">
+    <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/form" element={<VerificationForm/>}/>
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/OTP" element={<OTP />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/form" element={<VerificationForm />} />
           {/* <Route path="/verifydoc" element={<DocumentVerification/>}/> */}
           {/* <Route path="/admindashboard" element={<AdminDashboard/>}/> */}
-          <Route path="/UserDashboard" element={<UserDashboard/>}/>
+          <Route path="/UserDashboard" element={<UserDashboard />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </>
+
   );
 }
 
---
+++ src/pages/Firebase/Fireauth.js
@@ -0,0 +1,100 @@
+
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup} from "firebase/auth";
+import { auth, googleAuthProvider } from './Firebase';
+
+
+// async function otp_verification(phoneNumber) {
+//     const appVerifier = window.recaptchaVerifier;
+
+//     const auth = auth();
+//     signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+//         .then((confirmationResult) => {
+//             // SMS sent. Prompt user to type the code from the message, then sign the
+//             // user in with confirmationResult.confirm(code).
+//             window.confirmationResult = confirmationResult;
+//             // ...
+//         }).catch((error) => {
+//             // Error; SMS not sent
+//             // ...
+//         });
+// }
+
+// async function reCAPTCHA() {
+//     const auth = auth();
+//     window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
+//         'size': 'invisible',
+//         'callback': (response) => {
+//             // reCAPTCHA solved, allow signInWithPhoneNumber.
+//             // onSignInSubmit();
+//         }
+//     }, auth);
+// }
+
+
+async function register(email, password) {
+
+    await createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(email, password);
+    })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+            // ..
+            return false;
+        });
+    return true;
+}
+
+async function signIn(email, password) {
+    let a = 0;
+    await signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        console.log(auth.currentUser);
+        // Signed in 
+        const user = userCredential.user;
+        console.log(email, password);
+
+    })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+            a = 1;
+            // ..
+        });
+    if (a == 0) {
+        return true
+    }
+    else {
+        return false
+    }
+
+}
+
+async function signOut() {
+    await auth.signOut();
+}
+
+async function googleSignIn() {
+    googleAuthProvider.addScope('profile');
+    googleAuthProvider.addScope('email');
+    await signInWithPopup(auth, googleAuthProvider).then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+
+    })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+            // ..
+            return false;
+        });
+    return true;
+}
+
+
+
+export { signIn, signOut, register, googleSignIn};
