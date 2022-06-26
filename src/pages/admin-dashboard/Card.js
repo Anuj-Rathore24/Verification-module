6e9df9c55d6c9cdc4ae38fe4b7a4f5a7fa952d2a
Anuj-Rathore24 <anujsingh2409@gmail.com>
2022-06-26 23:11:32 +0530

    added modal

--- src/pages/admin-dashboard/Card.js
+++ src/pages/admin-dashboard/Card.js
@@ -1,19 +1,70 @@
-import React from "react";
-import styles from './../../styles/Card.css';
-import arrow from '../images/arrow.svg'
+import { React, useState } from "react";
+import "../../styles/Card.css";
+import { Button, Modal } from "react-bootstrap";
+import { Label } from "semantic-ui-react";
+
 export default function Card(props) {
+  //Use State for invoking close and open button
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div>
-      <div className={styles.main_card_container}>
-          <h2 className={styles.card_heading}>{props.date}</h2>  
-          <h2 className={styles.card_heading}>{props.queryId}</h2> 
-          <h2 className={styles.card_heading}>{props.name}</h2> 
-          <button className= {styles.card_button}>
-          <img src={arrow} alt="" /> 
-          </button>
-        
-        </div>
-      </div>
+      <div
+        className="main_card_container"
+        style={{
+          backgroundColor: `${props.color}`,
+          margin: 5,
+        }}
+      >
+        <h2 className="card_heading">{props.date}</h2>
+        <h2 className="card_heading">{props.queryId}</h2>
+        <h2 className="card_heading">{props.name}</h2>
 
+        {/* Modal for Showing Full Information */}
+
+        <Button variant="primary" onClick={handleShow}>
+          Show
+        </Button>
+
+        <Modal show={show} onHide={handleClose} animation={false}>
+
+          {/* Header for the modal */}
+          
+          <Modal.Header closeButton>
+            <Modal.Title>Id : {props.queryId}</Modal.Title>
+          </Modal.Header>
+
+          {/*Main Body of the Modal */}
+
+          <Modal.Body>
+            <div style={{display:"flex",flexDirection:"row"}}>
+              <p className="content">Name :</p>
+              <p className="content">{props.name}</p>
+            </div>
+            <div style={{display:"flex",flexDirection:"row"}}>
+              <p className="content">Date :</p>
+              <p className="content">{props.date}</p>
+            </div>
+            <div style={{display:"flex",flexDirection:"row"}}>
+              <p className="content">QueryId :</p>
+              <p className="content">{props.queryId}</p>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              View Documents
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Verify Documents
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    </div>
   );
 }
