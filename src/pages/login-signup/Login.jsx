003278ddb93cf8105842454a4cd53c60e0da8a7f
MonishK-1024 <kmonish72g@gmail.com>
2022-07-31 19:03:14 +0530

    design changes

--- src/pages/login-signup/Login.jsx
+++ src/pages/login-signup/Login.jsx
@@ -14,18 +14,6 @@ import AdminDashBoard from "../admin-dashboard/AdminDashBoard.js";
 export default function Login() {
   const navigate = useNavigate();
 
-  //Google Sign In
-  // async function clickFunctionGoogle() {
-  //     let result = false;
-  //     result = await FireAuth.googleSignIn();
-  //     console.log(result)
-  //     if (result) {
-  //         navigate("/UserDashboard");
-  //     } else {
-  //         alert("Error")
-  //     }
-  // }
-
   //Email-Password Signin
   async function clickFunctionSignin(email, password) {
     let result = "";
@@ -79,58 +67,12 @@ export default function Login() {
           <img src={logo} alt="MIT Logo" />
         </div>
 
-        {/* <div className="group63"
-                    style={{
-                        width: '40%',
-                        padding: '2% 3% 0% 3%',
-                        color: 'white',
-                    }}>
-                    <h1>Sign In to</h1>
-                    <h4>MIT-WPU Verification System</h4>
-                    <p style={{ fontSize: '13px' }}>University-side document verification is integral to every student's future academic and professional ventures as it  confirms the authenticity of details submitted by the student with regards to their academic program, CGPA course etc.</p>
-                </div> */}
-
         <div className="ls_signinCard">
           <Card className="ls_card">
             <Card.Body>
               <Card.Title className="ls_cardtitle">
                 <h1>Sign In</h1>
               </Card.Title>
-
-              {/* <Button className="ls_googleButton" onClick={() => clickFunctionGoogle()}
-                            variant="primary">
-
-                            <img src={google} className="ls_googleLogo" alt="Google" />  */}
-
-              {/* <span style={{
-                                color: 'blue',
-                                marginTop: '1.5%',
-                                fontSize: '15px',
-
-                            }}>Sign In with Google</span> */}
-              {/* </Button> */}
-
-              {/* <div className="line" style={{
-                            marginTop:'2%',
-                            display:'flex'
-                        }}>
-                        <hr style={{
-                            color:'#ffffff',
-                            width:'45%',
-                            height:'2px',
-                        }}/>
-                        <p style={{
-                            color:'white',
-                            marginTop:'2%',
-                            marginRight:'2%',
-                            marginLeft:'2%',
-                        }}>OR</p>
-                        <hr style={{
-                            color:'#ffffff',
-                            width:'45%',
-                            height:'2px',
-                        }}/>
-                        </div> */}
 
               <Form className="ls_loginForm" onSubmit={handleSubmit}>
                 <Form.Group
@@ -239,71 +181,6 @@ export default function Login() {
             </Card.Body>
           </Card>
         </div>
-
-        {/* <div className="SigninAs"
-                    style={{
-                        marginTop: '8%',
-                        marginLeft: '8%',
-                        height: '50%',
-                        width: '40%',
-                        // border:'2px solid black',
-                    }}>
-                    <div className='heading'>
-                        <span style={{
-                            padding: '3%',
-                            fontSize: '120%'
-                        }}>Sign In as</span>
-                    </div>
-
-                    <div className="profiles"
-                        style={{
-                            display: 'flex',
-                            flexWrap: 'wrap',
-                            paddingTop: '5%',
-                            justifyContent: 'space-between',
-                            gap: '2px'
-                        }}>
-                        <div className="profile1" style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            justifyContent: 'flex-start',
-                            width: '33%',
-                        }}>
-                            <img src={Admin} alt="Admin Login" style={{
-                                backgroundColor: 'white',
-                                borderRadius: '100%',
-                                border: '2px solid black',
-                            }} />
-                            <br />
-                            <span style={{
-                                width: '100%',
-                                textAlign: 'center',
-                            }}>Admin</span>
-                        </div>
-
-                        <div className="profile2" style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            justifyContent: 'flex-start',
-                            width: '33%',
-                        }}>
-                            <img src={Student} alt="Admin Login" style={{
-                                alignSelf: 'center',
-                                backgroundColor: 'white',
-                                borderRadius: '100%',
-                                border: '2px solid black',
-                            }} />
-                            <br />
-                            <span style={{
-                                width: '100%',
-                                textAlign: 'center',
-                            }}>Verification Agency/Student</span>
-                        </div>
-
-                    </div>
-                </div> */}
       </div>
       <div className="ls_bottomnav">
         <div className="ls_siteName">MIT-WPU Verification System :</div>
